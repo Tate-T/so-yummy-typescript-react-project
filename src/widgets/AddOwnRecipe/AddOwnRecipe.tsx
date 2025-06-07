857486e3455d19e17f8d9776fb93e7c06b57b3fb
Gorbunov Kirill <1kvgxiaomi1711@gmail.com>
2025-06-07 12:57:41 +0300

    init

---
+++ src/widgets/AddOwnRecipe/AddOwnRecipe.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import Container from '@/shared/Container/Container';
+import css from './addOwnRecipe.module.scss';
+
+export default ({}) => {
+    return (<section className={css.addOwnRecipe}><Container>
+        <h1 className={css.title}>Add recipe</h1>
+        
+    </Container></section>);
+};
