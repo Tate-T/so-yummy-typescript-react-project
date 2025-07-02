5cd64713e33ea6d549f36d9dd6fa072be2f5310d
StepanRychikhin2 <111581882+StepanRychikhin2@users.noreply.github.com>
2025-07-02 19:34:36 +0300

    save

--- src/widgets/Recipes/RecipeHero/RecipeHero.tsx
+++ src/widgets/Recipes/RecipeHero/RecipeHero.tsx
@@ -7,55 +7,71 @@ import { RootState } from "@/redux/store";
 import { useParams } from "next/navigation";
 import clock from "../../../../public/recipe/clock.svg";
 import { RecipeItem } from "@/entities/Recipe.type";
-import { useToggleFavorite } from "@/redux/apis/favoriteApi";
-import { useState } from "react";
+import { useToggleFavorite, useGetFavorite } from "@/redux/apis/favoriteApi";
+import { useState, useEffect } from "react";
 import MotivationCard from "@/shared/MotivationCard/MotivationCard";
-import parhImg from "../../../../public/motivationImgs/motivationImg4.jpg";
-import parhImgTwo from "../../../../public/motivationImgs/motivationImg3.jpg";
+
 export default function RecipeHero() {
   const { id }: { id: string } = useParams();
+  const { data: dataFav } = useGetFavorite(1); // отримуємо список улюблених
+  const favoriteTotal = dataFav?.total || 0;
+
   const data = useSelector(
     (state: RootState) =>
       state.recipes.queries[`getRecipe("${id}")`]?.data as RecipeItem | undefined,
   );
-  const [numToFaforites, setNumToFaforites] = useState(0);
-  const [toggleFavorite] = useToggleFavorite();
-  if (!data) {
-    return <></>;
-  }
 
-  function addNum(id: string) {
+  const [toggleFavorite] = useToggleFavorite();
+  const [showFirstCard, setShowFirstCard] = useState(false);
+  const [showTenthCard, setShowTenthCard] = useState(false);
+
+  useEffect(() => {
+    const firstShown = localStorage.getItem("firstFavoriteCardShown") === "true";
+    const tenthShown = localStorage.getItem("tenthFavoriteCardShown") === "true";
+
+    if (favoriteTotal === 1 && !firstShown) {
+      setShowFirstCard(true);
+      localStorage.setItem("firstFavoriteCardShown", "true");
+    }
+
+    if (favoriteTotal === 10 && !tenthShown) {
+      setShowTenthCard(true);
+      localStorage.setItem("tenthFavoriteCardShown", "true");
+    }
+  }, [favoriteTotal]);
+
+  if (!data) return null;
+
+  const handleAddFavorite = () => {
     toggleFavorite(id);
-    setNumToFaforites((prev) => prev + 1);
-    console.log(numToFaforites);
-  }
+  };
 
   return (
     <section className={css.section}>
       <Container>
-        {numToFaforites === 1 && (
+        {showFirstCard && (
           <MotivationCard
             title="You have added the first recipe to your favorites!"
-            imgPath={"/motivationImgs/motivationImg4.jpg"}
+            imgPath="/motivationImgs/motivationImg4.jpg"
           />
         )}
 
-        {numToFaforites === 10 && (
+        {showTenthCard && (
           <MotivationCard
             title="You have added 10 recipes to your favorites!"
-            imgPath={"/motivationImgs/motivationImg3.jpg"}
+            imgPath="/motivationImgs/motivationImg3.jpg"
           />
         )}
 
         <div className={css.recipeHeader}>
-          <h1 className={css.title}>{data.title ? data.title : "yuy"}</h1>
-          <p className={css.pidTitle}>{data!.description}</p>
-          <button onClick={() => id && addNum(id)} className={css.titleButton}>
+          <h1 className={css.title}>{data.title}</h1>
+          <p className={css.pidTitle}>{data.description}</p>
+          <button onClick={handleAddFavorite} className={css.titleButton}>
             Add to favorite recipes
           </button>
           <div className={css.timeBox}>
-            <Image alt="awd" className={css.svgTime} src={clock} />
-            <p className={css.txtTime}>{data!.time} min</p>
+            <Image alt="clock" className={css.svgTime} src={clock} />
+            <p className={css.txtTime}>{data.time} min</p>
           </div>
         </div>
       </Container>
